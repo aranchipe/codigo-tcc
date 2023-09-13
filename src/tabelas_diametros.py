25cc35efd4933444820ed797211e6c2f56b21f8a
Lucas Aranchipe <aranchipe98@gmail.com>
2023-09-12 21:52:48 -0300

    commit

--- src/tabelas_diametros.py
+++ src/tabelas_diametros.py
@@ -1,51 +1,53 @@
-diametros_serie_40 = [
-     {d_nominal: 0.5,
-        d_int: 15.76
-    },
-    {d_nominal: 0.75,
-        d_int: 20.96
-    },
-    {d_nominal: 1,
-        d_int: 26.64
-    },
-    {d_nominal: 1.25,
-        d_int: 35.08
-    },
-    {d_nominal: 1.5,
-        d_int: 40.94
-    },
-    {d_nominal: 2,
-        d_int: 52.48
-    },
-    {d_nominal: 2.5,
-        d_int: 62.68
-    },
-    {d_nominal: 3,
-        d_int: 77.92
-    },
-    {d_nominal: 3.5,
-        d_int: 90.12
-    },
-    {d_nominal: 4,
-        d_int: 102.26
-    },
-    {d_nominal: 5,
-        d_int:  128.2
-    },
-    {d_nominal: 6,
-        d_int: 154.08
-    },
-    {d_nominal: 8,
-        d_int: 202.74
-    },
-    {d_nominal: 10,
-        d_int: 254.51
-    },
-    {d_nominal: 12,
-        d_int: 303.22
-    },
+import math
 
-]
+diametros_serie_40 = {
+    0.5: {
+        "d_int": 15.76
+    },
+    0.75: {
+        "d_int": 20.96
+    },
+    1: {
+        "d_int": 26.64
+    },
+    1.25: {
+        "d_int": 35.08
+    },
+    1.5: {
+        "d_int": 40.94
+    },
+    2: {
+        "d_int": 52.48
+    },
+    2.5: {
+        "d_int": 62.68
+    },
+    3: {               
+        "d_int": 77.92
+    },
+    3.5: {                
+        "d_int": 90.12
+    },
+    4: {                
+        "d_int": 102.26
+    },
+    5: {               
+        "d_int": 128.2
+    },
+    6: {                
+        "d_int": 154.08
+    },
+    8: {               
+        "d_int": 202.74
+    },
+    10: {              
+        "d_int": 254.51
+    },
+    12: {       
+        "d_int": 303.22
+    }
+
+}
 
 
 diametros_serie_80 = {
@@ -97,4 +99,27 @@ diametros_serie_80 = {
 
 }
 
-print(diametros_serie_40[12])
+""" print(diametros_serie_40[6]['d_int']) """
+
+L1 = 4  # m
+L2 = 88  # m
+L3 = 75  # m
+L4 = 7  # m
+Q = 200  # m^3/h
+Q_real = Q/3600
+H1 = 0.85  # m
+H2 = 13.70  # m
+P1 = 316  # kPa
+hr = 9  # m
+Pr = 70.3  # kPa
+peso_esp = 9.5  # N/dm^3
+visc = 5.5  # stokes
+L_EQUIV = 215 # m
+
+# calculo de P2
+P2 = Pr + hr*peso_esp
+
+# calculo da energia disponivel
+energ_disp = (H1 + P1/peso_esp) - (H2 + P2/peso_esp)
+
+print(round(energ_disp,2))
